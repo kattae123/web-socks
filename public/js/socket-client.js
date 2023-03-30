8ffa404c062fcf2af91b2a584f5d50e72fc6df82
kattae123 <kattae.kattae@gmail.com>
2023-03-30 06:18:34 -0400

    websock example

---
+++ public/js/socket-client.js
@@ -0,0 +1,46 @@
+//ref of the html
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMessage = document.querySelector('#txtMessage');
+const btnSend = document.querySelector('#btnSend');
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    // console.log('Connected');
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+});
+
+socket.on('disconnect', () => {
+
+    console.log('Disconnected from the server');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+
+});
+
+socket.on('send-message', (payload) => {
+    console.log(payload);
+});
+
+btnSend.addEventListener('click', () => {
+
+    const message = txtMessage.value;
+
+    const payload = {
+        message,
+        id: '123ABC',
+        date: new Date().getTime()
+    };
+
+    socket.emit('send-message', payload,  ( id ) => {
+        console.log('From the server', id);
+    });
+
+});
+
---
+++ sockets/controller.js
@@ -0,0 +1,27 @@
+
+
+
+const socketController = (socket) => {
+
+    console.log('Client connected', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected', socket.id);
+    });
+
+    socket.on('send-message', ( payload, callback ) => {
+        
+        const id = 123456;
+
+        callback(id);
+
+        socket.broadcast.emit('send-message', payload);
+
+    });
+
+};
+
+
+module.exports = {
+    socketController
+};
